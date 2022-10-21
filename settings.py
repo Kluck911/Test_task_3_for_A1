29701a239debbb16eae888fd4a2b46f1406065dc
H_Zavadski <kluck1812@gmail.com>
2022-10-21 18:55:14 +0300

    add settings on git

---
+++ settings.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self, phone, password):
+        self.login = phone
+        self.passwrd = password
+
+
+test_user = User(phone="111111111", password="aaaaaa")
+valid_user = User(phone="XXXXXXXXX", password="XXXXXXXX")
