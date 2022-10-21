d9b232858795047089a44f40d1dbd7619674f47e
H_Zavadski <kluck1812@gmail.com>
2022-10-21 13:16:25 +0300

    step 2 added + locators were refactored

--- pages/shop_page.py
+++ pages/shop_page.py
@@ -1,14 +1,17 @@
 from pages.base_page import WebPage
 from pages.elements import ManyWebElements
+from pages.elements import WebElement
 
 
 
-class ShopPage(WebPage):
+class ShopPages(WebPage):
+
 
     def __init__(self, web_driver):
         url = 'https://www.a1.by/ru/c/shop'
         super().__init__(web_driver, url)
 
     list_stock_phone_btn = ManyWebElements(xpath="//*[@id='promo-product-button_0']")
-    btn = ''
+    list_phone_summary = ManyWebElements(xpath="//*[@id='promo-product-button_0']/../..//h3")
+    cookie_btn = WebElement(css_selector=".cookie-panel-button")
 
--- tests/test_shop.py
+++ tests/test_shop.py
@@ -1,22 +1,19 @@
 from time import sleep
+import random
 
-from pages.shop_page import ShopPage
-
-
-class TestsAddPhoneToCart:
-
-    def test_open_shop_page(self, web_browser):
-
-        page = ShopPage(web_browser)
-
-        assert page.get_current_url() == 'https://www.a1.by/ru/c/shop'
-
-        sleep(3)
-
-        print(len(page.list_stock_phone_btn.get_text()))
-
-        page.list_stock_phone_btn[1].click()
-
-        sleep(1)
+from pages.shop_page import ShopPages
 
 
+class TestsA1Shop:
+
+    def test_add_stock_rnd_phone_to_cart(self, web_browser):
+
+        page = ShopPages(web_browser)
+        assert page.get_current_url() == 'https://www.a1.by/ru/c/shop', 'shop page url error'
+
+        rnd_number = random.randint(0, len(page.list_stock_phone_btn.get_text())-1)  # random number for phone block
+        page.cookie_btn.click()  # close cookie message
+        start_page_phone_summary = page.list_phone_summary.get_text()[rnd_number]  # save smartphone summary
+        page.list_stock_phone_btn[rnd_number].click()
+        print(start_page_phone_summary)
+        assert page.get_current_url() == 'https://www.a1.by/ru/shop/phones/smartphones*'
