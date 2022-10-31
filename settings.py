ff3ea131b19575d04e957e2eff815de428e87f69
H_Zavadski <56726481+Kluck911@users.noreply.github.com>
2022-11-01 00:57:11 +0300

    Delete settings.py

--- settings.py
+++
@@ -1,8 +0,0 @@
-class User:
-    def __init__(self, phone, password):
-        self.login = phone
-        self.passwrd = password
-
-
-test_user = User(phone="111111111", password="aaaaaa")
-valid_user = User(phone="259397578", password="klicKKluck_1812")
